2259ab7dc9880b6c1ffd88a99b72f469bff5b578
François Doussin <doussin.f@gmail.com>
2021-07-08 15:40:01 +0200

    initial commit: project structure :building_construction:

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './assets/logo.png';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div class="app">
+      <img src={logo} alt="WildCodeSchool Logo"/>
+      <h1>Hello Wilders 👋</h1>
+      <p>Welcome in your project 2 structure</p>
+    </div>
+  );
+}
+
+export default App;
