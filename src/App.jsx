4e8a03a6309021966749aabb04a4d9d8f0ca13f3
mystere974 <m.loghann@gmail.com>
2021-07-12 13:40:52 +0200

    correction d'une erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import logo from './assets/logo.png';
+import Intro from './components/Intro';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div class="app">
-      <img src={logo} alt="WildCodeSchool Logo"/>
-      <h1>Hello Wilders 👋</h1>
-      <p>Welcome in your project 2 structure</p>
-    </div>
+<div>
+
+   <button>ENTER</button>
+
+</div>
+
+
   );
 }
 
---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+import "./Intro.css";
+
+function Intro() {
+    return (
+        
+<div>
+
+    
+    <img src="../assets/logo-site.png" class="logo-intro" alt="logo du site" />
+    <button>ENTER</button>
+    
+</div>
+
+
+    )
+
+    }
+export default Intro;
