205060ae23e3d4c8676f2fab005dca55e1faef33
Remy Chazelas <remychazelas@gmail.com>
2021-07-13 17:51:03 +0200

    API change, fetch reset

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,27 +7,7 @@ import "./Home.css";
 const Home = () => {
 
 
-    const [movies, setMovies] = useState([]);
-    const [searchValue, setSearchValue] = useState('');
 
-    const getMovieRequest = async (searchValue) => {
-        const url = `http://www.omdbapi.com/?i=${searchValue}&apikey=4680dd09`;
-
-        const response = await fetch(url);
-        const responseJson = await response.json();
-
-        if (responseJson.Search) {
-            setMovies(responseJson.Search);
-        }
-    };
-
-    useEffect(() => {
-        getMovieRequest(searchValue);
-    }, [searchValue]);
-
-    fetch(`http://www.omdbapi.com/?apikey=4680dd09&`).then((res) => {
-        console.log(res.json());
-    })
 
     return (
         <div className="home">
