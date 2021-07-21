e804808f9913412d7da539a584015a7b87f618c2
Shawn <shawn17@live.fr>
2021-07-21 15:43:36 +0200

    searchBar creation

---
+++ src/components/Notfound/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./NotFound.css"
+
+
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <Navbar />
+            <h1 className="NotFoundTitle">404</h1>
+            <h2 className="NotFoundTitle2">Hélas</h2>
+            <p className="NotFoundParagraphe">la page que vous cherchez regarde actuellement un film...</p>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import logo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.png";
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+
+
+
+const Navbar = () => {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false)
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar)
+    }, []);
+
+    return (
+        <div className={`nav ${show && "navBlack"}`}>
+            <div className="navContents">
+                <img
+                    className='navLogo'
+                    src={logo}
+                    alt='logo' />
+
+                <img
+                    className='navAvatar'
+                    src={avatar}
+                    alt='avatar' />
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
