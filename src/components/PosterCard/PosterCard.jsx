4f80f9423d5024cf572967a39230ec4c9649cb64
LauraTrehout <lauratrehout@gmail.com>
2021-07-23 11:32:53 +0200

    fixed scroll + link to moviecard on searchresult added

--- src/components/PosterCard/PosterCard.jsx
+++ src/components/PosterCard/PosterCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import { BsStar } from "react-icons/bs";
 import './PosterCard.css';
@@ -9,17 +9,19 @@ const [showRate, setShowRate] = useState(false);
 return (
     <>
     <div className="poster-card">
-        {/* <Link to={`/movie-card/${poster.id}`}> */}
+        {/* <NavLink to={`/movie-card/${poster.id}`} className='poster-img'> */}
         <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`} 
         className='poster-img' alt='poster'
         onMouseEnter={() => setShowRate(true)}
         onMouseLeave={() => setShowRate(false)} />
-        {/* </Link> */}
         {showRate && (
+            <>
             <div className="rate-overlay">
             <BsStar />  {poster.vote_average}/10
             </div>
+            </>
         )}
+        {/* </NavLink> */}
     </div>
     </>
     );
