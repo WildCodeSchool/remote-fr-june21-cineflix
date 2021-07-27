b6c4656e30f8a8c049006b03ebb5a4a9e0b2f095
RomainM81 <dcromain112@gmail.com>
2021-07-27 09:55:19 +0100

    add navlink on postercard

--- src/components/PosterCard/PosterCard.jsx
+++ src/components/PosterCard/PosterCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import { BsStar } from "react-icons/bs";
 import './PosterCard.css';
@@ -7,19 +7,19 @@ const PosterCard = ({ poster }) => {
 const [showRate, setShowRate] = useState(false);
 
 return (
-    <>
     <div className="poster-card">
-        <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`} 
-        className='poster-img' alt='poster'
-        onMouseEnter={() => setShowRate(true)}
-        onMouseLeave={() => setShowRate(false)} />
-        {showRate && (
-            <div className="rate-overlay">
-            <BsStar />  {poster.vote_average}/10
-            </div>
-        )}
+        <NavLink to={`/movie-card/${poster.id}`} className="poster-img">
+            <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`} 
+            className='poster-img' alt='poster'
+            onMouseEnter={() => setShowRate(true)}
+            onMouseLeave={() => setShowRate(false)} />
+            {showRate && (
+                <div className="rate-overlay">
+                <BsStar />  {poster.vote_average}/10
+                </div>
+            )}
+        </NavLink>
     </div>
-    </>
     );
 }
 
