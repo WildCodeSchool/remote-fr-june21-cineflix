1eb045538dac0b82b74361367bcf8014b4416642
LauraTrehout <lauratrehout@gmail.com>
2021-07-27 10:54:40 +0200

    navlink in postercard

--- src/components/PosterCard/PosterCard.jsx
+++ src/components/PosterCard/PosterCard.jsx
@@ -4,26 +4,26 @@ import { BsStar } from "react-icons/bs";
 import './PosterCard.css';
 
 const PosterCard = ({ poster }) => {
-const [showRate, setShowRate] = useState(false);
+    const [showRate, setShowRate] = useState(false);
 
-return (
-    <>
-    <div className="poster-card">
-        {/* <NavLink to={`/movie-card/${poster.id}`} className='poster-img'> */}
-        <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`} 
-        className='poster-img' alt='poster'
-        onMouseEnter={() => setShowRate(true)}
-        onMouseLeave={() => setShowRate(false)} />
-        {showRate && (
-            <>
-            <div className="rate-overlay">
-            <BsStar />  {poster.vote_average}/10
+    return (
+        <>
+            <div className="poster-card">
+                <NavLink to={`/movie-card/${poster.id}`} className='poster-img'>
+                    <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`}
+                        className='poster-img' alt='poster'
+                        onMouseEnter={() => setShowRate(true)}
+                        onMouseLeave={() => setShowRate(false)} />
+                    {showRate && (
+                        <>
+                            <div className="rate-overlay">
+                                <BsStar />  {poster.vote_average}/10
+                            </div>
+                        </>
+                    )}
+                </NavLink>
             </div>
-            </>
-        )}
-        {/* </NavLink> */}
-    </div>
-    </>
+        </>
     );
 }
 
--- src/components/TopContainer/TopContainer.jsx
+++ src/components/TopContainer/TopContainer.jsx
@@ -3,47 +3,47 @@ import PosterCard from "../PosterCard/PosterCard";
 import './TopContainer.css';
 
 const TopContainer = () => {
-const [popularMovie, setPopularMovie] = useState([]);
-const [popularShow, setPopularShow] = useState([]);
+    const [popularMovie, setPopularMovie] = useState([]);
+    const [popularShow, setPopularShow] = useState([]);
 
-const apiKey = 'cda80ca49e23464f07b0b27ac89f1fdd';
+    const apiKey = 'cda80ca49e23464f07b0b27ac89f1fdd';
 
-useEffect (() => {
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr&page=1`)
-.then((res) => res.json())
-.then((data) => setPopularMovie(data.results));
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr&page=1`)
+            .then((res) => res.json())
+            .then((data) => setPopularMovie(data.results));
 
-}, [])
-
-useEffect (() => {
-    fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=fr&page=1`)
-    .then((res) => res.json())
-    .then((data) => setPopularShow(data.results));
-    
     }, [])
 
-const mapPoster = (state) => {
-    return ( 
-        <>
-        {state.map((poster, id) => 
-            <PosterCard key={poster.id} poster={poster} />)}
-        </>
-    );
-}
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=fr&page=1`)
+            .then((res) => res.json())
+            .then((data) => setPopularShow(data.results));
 
-return (
-    <>
-    <div className="top-container">
-        <h3>Top 20 Films</h3>
-        <div className="top-cards">
-            {mapPoster(popularMovie)}
-        </div> 
-        <h3>Top 20 Séries</h3>
-        <div className="top-cards">
-            {mapPoster(popularShow)}
-        </div>
-    </div>
-    </>
+    }, [])
+
+    const mapPoster = (state) => {
+        return (
+            <>
+                {state.map((poster, id) =>
+                    <PosterCard key={poster.id} poster={poster} />)}
+            </>
+        );
+    }
+
+    return (
+        <>
+            <div className="top-container">
+                <h3>Top 20 Films</h3>
+                <div className="top-cards">
+                    {mapPoster(popularMovie)}
+                </div>
+                <h3>Top 20 Séries</h3>
+                <div className="top-cards">
+                    {mapPoster(popularShow)}
+                </div>
+            </div>
+        </>
     );
 }
 
