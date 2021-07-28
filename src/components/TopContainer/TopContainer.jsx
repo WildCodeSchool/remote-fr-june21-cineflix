97f4671fc88385eced671a19e0631b1a72f6920f
LauraTrehout <lauratrehout@gmail.com>
2021-07-28 12:00:00 +0200

    adapted css for firefox

--- src/components/TopContainer/TopContainer.jsx
+++ src/components/TopContainer/TopContainer.jsx
@@ -32,7 +32,6 @@ const mapPoster = (state) => {
 }
 
 return (
-    <>
     <div className="top-container">
         <h3>Top 20 Films</h3>
         <div className="top-cards">
@@ -43,7 +42,6 @@ return (
             {mapPoster(popularShow)}
         </div>
     </div>
-    </>
     );
 }
 
