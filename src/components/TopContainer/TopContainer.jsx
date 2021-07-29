3b1ca2a8fd282a1db2741c3259ec4fe94e4efc1e
LauraTrehout <lauratrehout@gmail.com>
2021-07-29 16:00:16 +0200

    added function in useEffect

--- src/components/TopContainer/TopContainer.jsx
+++ src/components/TopContainer/TopContainer.jsx
@@ -9,40 +9,44 @@ const TopContainer = () => {
     const apiKey = 'cda80ca49e23464f07b0b27ac89f1fdd';
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr&page=1`)
-            .then((res) => res.json())
-            .then((data) => setPopularMovie(data.results));
-
+        const fetchMovies = () => {
+            fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`)
+                .then((res) => res.json())
+                .then((data) => setPopularMovie(data.results));
+        }
+        fetchMovies()
     }, [])
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=fr&page=1`)
-            .then((res) => res.json())
-            .then((data) => setPopularShow(data.results));
-
+        const fetchShows = () => {
+            fetch(`https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`)
+                .then((res) => res.json())
+                .then((data) => setPopularShow(data.results));
+        }
+        fetchShows()
     }, [])
 
 
     const mapPoster = (state) => {
-        return ( 
+        return (
             <>
-            {state.map((poster, id) => 
-                <PosterCard key={poster.id} poster={poster} />)}
+                {state.map((poster, id) =>
+                    <PosterCard key={poster.id} poster={poster} />)}
             </>
         );
     }
-    
-return (
-    <div className="top-container">
-        <h3>Top 20 Films</h3>
-        <div className="top-cards">
-            {mapPoster(popularMovie)}
-        </div> 
-        <h3>Top 20 Séries</h3>
-        <div className="top-cards">
-            {mapPoster(popularShow)}
+
+    return (
+        <div className="top-container">
+            <h3>Top 20 Films</h3>
+            <div className="top-cards">
+                {mapPoster(popularMovie)}
+            </div>
+            <h3>Top 20 Séries</h3>
+            <div className="top-cards">
+                {mapPoster(popularShow)}
+            </div>
         </div>
-    </div>
     );
 }
 
