89b0b23dfad373691b62fcd01cfe707c5e848cc9
LauraTrehout <lauratrehout@gmail.com>
2021-08-02 11:38:14 +0200

    horizontal scroll with arrow buttons

--- src/components/PosterCard/PosterCard.jsx
+++ src/components/PosterCard/PosterCard.jsx
@@ -3,20 +3,20 @@ import { useState } from 'react';
 import { BsStar } from "react-icons/bs";
 import './PosterCard.css';
 
-const PosterCard = ({ poster }) => {
+const PosterCard = ({ id, poster_path, vote_average, release_date, first_air_date }) => {
     const [showRate, setShowRate] = useState(false);
     
     return (
             <div className="poster-card">
-                <NavLink to={`/movie-card/${poster.id}`} className='poster-img'>
-                    <img src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`}
+                <NavLink to={`/movie-card/${id}`} className='poster-img'>
+                    <img src={`https://image.tmdb.org/t/p/w500${poster_path}`}
                         className='poster-img' alt='poster'
                         onMouseEnter={() => setShowRate(true)}
                         onMouseLeave={() => setShowRate(false)} />
                     {showRate && (
                             <div className="rate-overlay">
-                                <li><BsStar />  {poster.vote_average}/10</li>
-                                <li>{poster.release_date ? poster.release_date.slice(0, 4) : poster.first_air_date.slice(0, 4)}</li>
+                                <li><BsStar />  {vote_average}/10</li>
+                                <li>{release_date ? release_date.slice(0, 4) : first_air_date ? first_air_date.slice(0, 4) : null}</li>
                             </div>
                     )}
                 </NavLink>
