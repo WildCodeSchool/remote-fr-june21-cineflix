2fef7d30a7c936fb32b9f9d74b66fbf6c4ca1d83
RomainM81 <dcromain112@gmail.com>
2021-08-03 16:10:28 +0100

    chnaged RandomMovies fetch and added .map

--- src/components/RandomMovies/RandomMovies.jsx
+++ src/components/RandomMovies/RandomMovies.jsx
@@ -3,29 +3,37 @@ import { NavLink } from "react-router-dom";
 
 const RandomMovies = () => {
     const [RandMovies, setRandMovies] = useState([])
-    function getRandom() {
-        return Math.floor(Math.random() * 700000);
-    }
-
-    console.log(getRandom())
     const api_key = 'cda80ca49e23464f07b0b27ac89f1fdd'
+    // const [loader, setLoader] = useState(true)
 
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
     useEffect(() => {
-        for(let i = 0; i < 20; i++) {
+        for(let i = 0; i < 10; i++) {
         const getRandMovie = () => {
-            fetch(`https://api.themoviedb.org/3/movie/${getRandom()}?api_key=${api_key}&language=fr`)
+            fetch(`https://api.themoviedb.org/3/movie/${getRandom(1, 500000)}?api_key=${api_key}&language=fr`)
                 .then(response => response.json())
-                .then(data => data.status_code != 34 ? setRandMovies(RandMovies.push(data)) : getRandMovie())
-                console.log(RandMovies)
+                .then(data => data.status_code != 34 && data.adult === false && data.popularity > 10 && data.vote_average > 5 ? setRandMovies(RandMovies => [...RandMovies, data]) : getRandMovie())
             }
             getRandMovie()
+            // setLoader(false)
+            console.log(RandMovies)
         }
     }, [])
     
     
     return (
         <div>
-            {RandMovies.title ? console.log(RandMovies) : null }
+            {console.log(RandMovies)}
+            <h3> teeeeeeest {RandMovies[9] ? RandMovies.map((movie, index) => (
+                    <figure key={index}>
+                        <NavLink to={`/movie-card/${movie.id}`} >
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Affiche ${movie.title}`} />
+                            <p>{movie.title}</p>
+                        </NavLink>
+                    </figure>)) : null }</h3>
         </div>
     );
 }
