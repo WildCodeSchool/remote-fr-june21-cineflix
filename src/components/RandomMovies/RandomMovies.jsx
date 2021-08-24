5149730d903a7a62cc10bc259a186eb5f4b44e5b
RomainM81 <dcromain112@gmail.com>
2021-08-24 11:32:43 +0100

    added selection and fetch on TopContainer

--- src/components/RandomMovies/RandomMovies.jsx
+++ src/components/RandomMovies/RandomMovies.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 const RandomMovies = () => {
-    const [RandMovies, setRandMovies] = useState([])
-    const api_key = 'cda80ca49e23464f07b0b27ac89f1fdd'
+    // const [RandMovies, setRandMovies] = useState([])
+    // const api_key = 'cda80ca49e23464f07b0b27ac89f1fdd'
 
     // function getRandom(min, max) {
     //     return Math.floor(Math.random() * (max - min) + min);
@@ -21,56 +21,6 @@ const RandomMovies = () => {
     //     }
     // }, [])
 
-    const [selectCat, setSelectCat] = useState("")
-    const handeChange= (e) => {
-        setSelectCat(e.target.value) 
-    }
-    console.log(selectCat)
-
-    useEffect(() => {
-        const getMovieCat = () => {
-            fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${selectCat}&api_key=${api_key}&language=fr`)
-                .then(res => res.json())
-                .then(data => setRandMovies(data.results));
-        }
-        getMovieCat()
-    }, []);
-
-    return (
-        <div>
-            <select name="Catégorie" onChange={(e) => handeChange(e)}>
-                <option disabled selected>Choisir Catégorie</option>
-                <option select value='28'>Action</option>
-                <option select value='12'>Aventure</option>
-                <option select value='16'>Animation</option>
-                <option select value='35'>Comédie</option>
-                <option select value='80'>Crime</option>
-                <option select value='99'>Documentaire</option>
-                <option select value='18'>Drame</option>
-                <option select value='10751'>Familial</option>
-                <option select value='14'>Fantastique</option>
-                <option select value='36'>Historique</option>
-                <option select value='27'>Horreur</option>
-                <option select value='10402'>Musique</option>
-                <option select value='9648'>Mystère</option>
-                <option select value='10749'>Romance</option>
-                <option select value='878'>Science-Fiction</option>
-                <option select value='10770'>Téléfilm</option>
-                <option select value='53'>Thriller</option>
-                <option select value='10752'>Guerre</option>
-                <option select value='37'>Western</option>
-            </select>
-
-            {/* {console.log(RandMovies)}
-            <h3> Random {RandMovies[9] ? RandMovies.map((movie, index) => (
-                    <figure key={index}>
-                        <NavLink to={`/movie-card/${movie.id}`} >
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Affiche ${movie.title}`} />
-                            <p>{movie.title}</p>
-                        </NavLink>
-                    </figure>)) : null }</h3> */}
-        </div>
-    );
 }
 
 export default RandomMovies;
--- src/components/TopContainer/TopContainer.jsx
+++ src/components/TopContainer/TopContainer.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef } from "react";
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
 
 import PosterCard from "../PosterCard/PosterCard";
-import RandomMovies from '../../components/RandomMovies/RandomMovies'
 
 import './TopContainer.css';
 
@@ -11,7 +10,9 @@ const TopContainer = () => {
     const [popularShow, setPopularShow] = useState([]);
     const [slideNumber, setSlideNumber] = useState(0);
     const [upCo, setUpCo] = useState([])
-    
+    const [RandMovies, setRandMovies] = useState([])
+    const [selectCat, setSelectCat] = useState("")
+
     const apiKey = 'cda80ca49e23464f07b0b27ac89f1fdd';
 
     useEffect(() => {
@@ -42,6 +43,16 @@ const TopContainer = () => {
         getUpCo()
     }, [])
 
+    useEffect(() => {
+        const getMovieCat = () => {
+            fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${selectCat}&api_key=${apiKey}&language=fr`)
+                .then(res => res.json())
+                .then(data => setRandMovies(data.results));
+        }
+        getMovieCat()
+        console.log(RandMovies)
+    }, [selectCat]);
+
 
     const mapPoster = (state) => {
         return (
@@ -52,8 +63,14 @@ const TopContainer = () => {
         );
     }
 
+    const handeChange = (e) => {
+        setSelectCat(e.target.value)
+    }
+
     const listRefMovies = useRef();
     const listRefShows = useRef();
+    const listRefUpCo = useRef();
+    const listRefRand = useRef();
     const handleClick = (direction, type) => {
         let distance = type.current.getBoundingClientRect().x
 
@@ -85,18 +102,45 @@ const TopContainer = () => {
                 </div>
                 <ArrowForwardIosOutlined className="sliderArrowRight" onClick={() => handleClick('right', listRefShows)} />
             </div>
-            <h2>Prochaines Sorties</h2>
+            <h2>Récents</h2>
             <div className="top-cards">
-                <ArrowBackIosOutlined className="sliderArrowLeft" onClick={() => handleClick('left', listRefShows)} />
-                <div className="popularLists" ref={listRefShows} >
+                <ArrowBackIosOutlined className="sliderArrowLeft" onClick={() => handleClick('left', listRefUpCo)} />
+                <div className="popularLists" ref={listRefUpCo} >
                     {mapPoster(upCo)}
                 </div>
-                <ArrowForwardIosOutlined className="sliderArrowRight" onClick={() => handleClick('right', listRefShows)} />
+                <ArrowForwardIosOutlined className="sliderArrowRight" onClick={() => handleClick('right', listRefUpCo)} />
             </div>
             <div>
-                <RandomMovies />
+                <select defaultValue="Choisir Catégorie" className="selectB" name="Catégorie" onChange={(e) => handeChange(e)}>
+                    <option disabled selected>Choisir Catégorie</option>
+                    <option select value='28'>Action</option>
+                    <option select value='12'>Aventure</option>
+                    <option select value='16'>Animation</option>
+                    <option select value='35'>Comédie</option>
+                    <option select value='80'>Crime</option>
+                    <option select value='99'>Documentaire</option>
+                    <option select value='18'>Drame</option>
+                    <option select value='10751'>Familial</option>
+                    <option select value='14'>Fantastique</option>
+                    <option select value='36'>Historique</option>
+                    <option select value='27'>Horreur</option>
+                    <option select value='10402'>Musique</option>
+                    <option select value='9648'>Mystère</option>
+                    <option select value='10749'>Romance</option>
+                    <option select value='878'>Science-Fiction</option>
+                    <option select value='10770'>Téléfilm</option>
+                    <option select value='53'>Thriller</option>
+                    <option select value='10752'>Guerre</option>
+                    <option select value='37'>Western</option>
+                </select>
             </div>
-            
+            <div className="top-cards">
+                <ArrowBackIosOutlined className="sliderArrowLeft" onClick={() => handleClick('left', listRefRand)} />
+                <div className="popularLists" ref={listRefRand} >
+                    {mapPoster(RandMovies)}
+                </div>
+                <ArrowForwardIosOutlined className="sliderArrowRight" onClick={() => handleClick('right', listRefRand)} /> 
+            </div>
         </div>
     );
 }
